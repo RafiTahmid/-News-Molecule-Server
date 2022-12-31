96ef4039a66e436f1b02a1c02e4a7f04451cacd2
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-31 11:22:19 +0600

    Created News and category data loading API

--- index.js
+++ index.js
@@ -6,13 +6,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
-  res.send("Hellow World");
+  res.send("Hello World");
 });
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+  // console.log(req.params.id);
 });
 
 app.listen(port, () => {
